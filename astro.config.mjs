16d8dd4f586cdc8fc263ada73e94cf7737f742b5
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-10-12 19:57:19 -0400

    add website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://carlosdlc.dev/',
   integrations: [tailwind(), react(), image()]
 })
