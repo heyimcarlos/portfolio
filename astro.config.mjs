f5e13fb37d82442c9dfa00d89615cc7cc11a5d24
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-10-12 19:26:53 -0400

    installing image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+  integrations: [tailwind(), react(), image()]
+})
