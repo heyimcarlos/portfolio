b3ecf4e76aeca772d5fb621110cb8ba2c3faa42c
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-10-12 19:54:40 -0400

    Cleaning up styles and setting up tailwind config with new font

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Carlos de la Cruz - Software Engineer'
+export const SITE_DESCRIPTION =
+  'Personal website of Carlos de la Cruz, a software engineer based in London, UK.'
+export const SITE_URL = 'https://carlosdelacruz.dev'
