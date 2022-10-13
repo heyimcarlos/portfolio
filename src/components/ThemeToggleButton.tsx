ae448417442ccdac3fa91becb1b03f90f01bbdaf
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-10-12 22:51:19 -0400

    create light/dark toggle button

---
+++ src/components/ThemeToggleButton.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+
+const themes = ['light', 'dark']
+
+export default function ThemeToggle() {
+  console.log('import', import.meta)
+  const [isMounted, setIsMounted] = useState(false)
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined
+    }
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+      return localStorage.getItem('theme')
+    }
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark'
+    }
+    return 'light'
+  })
+  const toggleTheme = () => {
+    const t = theme === 'light' ? 'dark' : 'light'
+    localStorage.setItem('theme', t)
+    setTheme(t)
+  }
+
+  useEffect(() => {
+    const root = document.documentElement
+    if (theme === 'light') {
+      root.classList.remove('dark')
+    } else {
+      root.classList.add('dark')
+    }
+  }, [theme])
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  return isMounted ? <div className="inline-flex">Button</div> : <div />
+}
