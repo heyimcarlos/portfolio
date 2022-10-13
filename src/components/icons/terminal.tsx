2bbbe1c6e8b685fe9eac6cfe5d48a69bd543fe1e
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-10-13 19:21:42 -0400

    add terminal icon and remove terminal images from public

---
+++ src/components/icons/terminal.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function TerminalIcon() {
+  return (
+    <svg
+      className="w-6 inline-block transition-transform group-hover:rotate-[20deg]"
+      viewBox="0 0 25 25"
+      fill="currentColor"
+    >
+      <g>
+        <g>
+          <g>
+            <path
+              fill="currentColor"
+              d="M19,24H5c-2.8,0-5-2.2-5-5V5c0-2.8,2.2-5,5-5h14c2.8,0,5,2.2,5,5v14C24,21.8,21.8,24,19,24z M5,2C3.3,2,2,3.3,2,5v14
+               c0,1.7,1.3,3,3,3h14c1.7,0,3-1.3,3-3V5c0-1.7-1.3-3-3-3H5z"
+            />
+          </g>
+        </g>
+        <g>
+          <g>
+            <path d="M9,10c-0.3,0-0.5-0.1-0.7-0.3l-3-3c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0l3,3c0.4,0.4,0.4,1,0,1.4C9.5,9.9,9.3,10,9,10z" />
+          </g>
+        </g>
+        <g>
+          <g>
+            <path d="M6,13c-0.3,0-0.5-0.1-0.7-0.3c-0.4-0.4-0.4-1,0-1.4l3-3c0.4-0.4,1-0.4,1.4,0s0.4,1,0,1.4l-3,3C6.5,12.9,6.3,13,6,13z" />
+          </g>
+        </g>
+        <g>
+          <g>
+            <path d="M15,13h-4c-0.6,0-1-0.4-1-1s0.4-1,1-1h4c0.6,0,1,0.4,1,1S15.6,13,15,13z" />
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
