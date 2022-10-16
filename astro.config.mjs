7d5eb87e842ea52bc2cccec355f45c8102cc0ff8
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-10-16 14:32:54 -0400

    re-install packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import image from '@astrojs/image';
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import image from '@astrojs/image'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://carlosdlc.dev/',
-  integrations: [tailwind(), react(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
-  output: "server",
+  // site: 'https://carlosdlc.dev/',
+  integrations: [
+    tailwind(),
+    react(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    })
+  ],
+  output: 'server',
   adapter: vercel()
-});
+})
