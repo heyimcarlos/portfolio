3b3c6e8bf30ca1a7027e588fecd23149b957f9ac
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-10-16 13:55:44 -0400

    add astro vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import image from '@astrojs/image'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import image from '@astrojs/image';
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://carlosdlc.dev/',
-  integrations: [
-    tailwind(),
-    react(),
-    image({ serviceEntryPoint: '@astrojs/image/sharp' })
-  ]
-})
+  integrations: [tailwind(), react(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
+  output: "server",
+  adapter: vercel()
+});
