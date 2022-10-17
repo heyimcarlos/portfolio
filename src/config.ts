abf2facf8a4d31ae30bccb98ef3e78913e86481e
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-10-16 22:00:23 -0400

    edit site title and description

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
-export const SITE_TITLE = 'Carlos de la Cruz - Software Engineer'
-export const SITE_DESCRIPTION =
-  'Personal website of Carlos de la Cruz, a software engineer.'
-export const SITE_URL = 'https://carlosdelacruz.dev'
+export const SITE_TITLE = 'Carlos de la Cruz'
+export const SITE_DESCRIPTION = 'Carlos&aois; personal website.'
+export const SITE_URL = 'https://heyimcarlos.dev'
 export const SOURCE_URL = 'https://github.com/heyimcarlos/portfolio'
