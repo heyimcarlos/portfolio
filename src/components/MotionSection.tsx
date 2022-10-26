e5fcc2d9996eaf0bb1552f81651293bc5bd40393
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-10-26 18:58:10 -0400

    motion component now animates only if user had reduced animation disabled in his settings

--- src/components/MotionSection.tsx
+++ src/components/MotionSection.tsx
@@ -1,5 +1,5 @@
-import { motion, useReducedMotion } from 'framer-motion'
-import React, { ReactNode } from 'react'
+import { motion, MotionConfig, useReducedMotion } from 'framer-motion'
+import React, { ReactNode, useEffect, useState } from 'react'
 
 type Props = {
   delay?: number
@@ -13,17 +13,31 @@ export default function MotionSection({
   classNames
 }: Props) {
   const shouldReduceMotion = useReducedMotion()
+  const [isMounted, setIsMounted] = useState(false)
 
-  return shouldReduceMotion ? (
-    <div className={`mb-6 ${classNames}`}>{children}</div>
-  ) : (
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  return isMounted && shouldReduceMotion ? (
     <motion.div
-      initial={{ y: 10, opacity: 0 }}
+      initial={{ y: 0, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.8, delay }}
+      transition={{ duration: 0, delay: 0 }}
       className={`mb-6 ${classNames}`}
     >
       {children}
     </motion.div>
+  ) : (
+    <MotionConfig reducedMotion="user">
+      <motion.div
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay }}
+        className={`mb-6 ${classNames}`}
+      >
+        {children}
+      </motion.div>
+    </MotionConfig>
   )
 }
