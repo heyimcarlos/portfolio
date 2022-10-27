5d2523157908c0f936011df2634bb86a92b39dcb
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-10-27 17:40:19 -0400

    import fonts with fontsource

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 
 type Props = {
   children: any
@@ -16,7 +16,7 @@ export default function Heading({
 }: Props) {
   const variantClasses =
     variant === 'section-title'
-      ? 'font-mplus font-bold text-xl mb-4 mt-3 leading-[1.33] md:leading-tight underline underline-offset-[6px] decoration-4 decoration-custom-teal dark:decoration-zinc-600'
+      ? 'font-mplus font-semibold text-xl mb-4 mt-3 leading-[1.33] md:leading-tight underline underline-offset-[6px] decoration-4 decoration-custom-teal dark:decoration-zinc-600'
       : ''
   const heading = React.createElement(as, {
     className: ` ${variantClasses}
