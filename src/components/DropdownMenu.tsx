a54148954a1a45368c17ea3456f40b16a3881390
Carlos De la Cruz <me@heyimcarlos.dev>
2024-04-25 19:52:39 -0400

    chore: view resume

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -43,7 +43,7 @@ export default function DropdownMenu() {
                     >
                         <button
                             className="rounded-md w-full border-t border-zinc-400 dark:border-zinc-700 px-2 py-2 text-sm font-medium shadow-sm hover:bg-zinc-400 dark:hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-custom-blue focus:ring-offset-2 focus:ring-offset-gray-100 transition-all scale-100"
-                        >Download Resume</button
+                        >View Resume</button
                         >
                     </a>
                 </Menu.Items>
